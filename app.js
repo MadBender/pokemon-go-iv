bf3b144197e93c07b5bd2ffb934a4f807c369e68
Artem vasilyev <ryder86@gmail.com>
2016-10-17 20:08:18 +0300

    formatted to 4 space indents

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 'use strict';
+
 const process = require('process');
 const PogoBuf = require("pogobuf");
 const pad = require("pad");
@@ -9,93 +10,94 @@ const levelMultipliers = require("./levelMultipliers.js");
 const argv = require('minimist')(process.argv.slice(2));
 
 if (argv.h || argv.help) {
-  console.log('Usage: ');
-  console.log('node app.js -u <username> -p <password> -a <ptc|google> [-s name|cp|hp|attack|defence|stamina|iv]');
-  process.exit();
+    console.log('Usage: ');
+    console.log('node app.js -u <username> -p <password> -a <ptc|google> [-s name|cp|hp|attack|defence|stamina|iv]');
+    process.exit();
 }
 
 const username = argv.u;
 const password = argv.p;
 
 if (!username || !password) {
-  console.error('Username and password required (-u and -p)');
-  process.exit(1);
+    console.error('Username and password required (-u and -p)');
+    process.exit(1);
 }
 
 const provider = argv.a;
 const sort = argv.s || 'iv';
 
 const login = provider == "ptc"
-  ? new PogoBuf.PTCLogin()
-  : new PogoBuf.GoogleLogin()
+    ? new PogoBuf.PTCLogin()
+    : new PogoBuf.GoogleLogin()
 
 //working
 login.login(username, password)
-  .then(token => {
-    const client = new PogoBuf.Client();
-    client.setAuthInfo(provider, token);
-    client.init();
-    return client.getInventory(0);
-  })
-  .then(resp => {
-    //processing inventory
-    const items = resp.inventory_delta.inventory_items;
-    let pokemons = items.filter(
-      item => item.inventory_item_data
-        && item.inventory_item_data.pokemon_data
-        && item.inventory_item_data.pokemon_data.pokemon_id
-      )
-      .map(item => {
-        const p = item.inventory_item_data.pokemon_data;
-        const res = {
-          name: p.nickname || pokemonNames[p.pokemon_id] || "#" + p.pokemon_id,
-          cp: p.cp,
-          hp: p.stamina_max,
-          attack: p.individual_attack || 0,
-          defence: p.individual_defense || 0,
-          stamina: p.individual_stamina || 0
-        };
-        const cpMult = p.cp_multiplier + p.additional_cp_multiplier;
-        const levelMult = levelMultipliers.find(m => Math.abs(m.multiplier - cpMult) < 1e-4);
-        if (levelMult) {
-          res.level = levelMult.level;
+    .then(token => {
+        const client = new PogoBuf.Client();
+        client.setAuthInfo(provider, token);
+        client.init();
+        return client.getInventory(0);
+    })
+    .then(resp => {
+        //processing inventory
+        const items = resp.inventory_delta.inventory_items;
+        let pokemons = items.filter(
+            item => item.inventory_item_data
+                && item.inventory_item_data.pokemon_data
+                && item.inventory_item_data.pokemon_data.pokemon_id
+            )
+            .map(item => {
+                const p = item.inventory_item_data.pokemon_data;
+                const res = {
+                    name: p.nickname || pokemonNames[p.pokemon_id] || "#" + p.pokemon_id,
+                    cp: p.cp,
+                    hp: p.stamina_max,
+                    attack: p.individual_attack || 0,
+                    defence: p.individual_defense || 0,
+                    stamina: p.individual_stamina || 0
+                };
+                const cpMult = p.cp_multiplier + p.additional_cp_multiplier;
+                const levelMult = levelMultipliers.find(m => Math.abs(m.multiplier - cpMult) < 1e-4);
+                if (levelMult) {
+                    res.level = levelMult.level;
+                }
+                res.iv = (res.attack + res.defence + res.stamina) / 45 * 100;
+                return res;
+            });
+
+        //sorting
+        const sortFields = [sort];
+        //sorting in descending order so best pokemons will be at the top
+        const sortDirs = [sort == "name" ? "asc" : "desc"];
+        if (sort != "cp") {
+            sortFields.push("cp");
+            sortDirs.push("desc");
         }
-        res.iv = (res.attack + res.defence + res.stamina) / 45 * 100;
-        return res;
-      });
 
-    //sorting
-    const sortFields = [sort];
-    //sorting in descending order so best pokemons will be at the top
-    const sortDirs = [sort == "name" ? "asc" : "desc"];
-    if (sort != "cp") {
-        sortFields.push("cp");
-        sortDirs.push("desc");
-    }
+        pokemons = _.orderBy(pokemons, sortFields, sortDirs);
 
-    pokemons = _.orderBy(pokemons, sortFields, sortDirs);    
+        //rendering
+        console.log();
+        const renderRow = (name, cp, hp, level, att, def, sta, iv) =>
+            console.log(
+                pad(name, 20)
+                + pad(5, cp)
+                + pad(4, iv)
+                + pad(6, level || "")
+                + pad(7, hp)
+                + pad(7, att)
+                + pad(4, def)
+                + pad(4, sta)
+            );
 
-    //rendering
-    console.log();
-    const renderRow = (name, cp, hp, level, att, def, sta, iv) =>
-      console.log(
-        pad(name, 20)
-        + pad(5, cp)
-        + pad(4, iv)
-        + pad(6, level || "")
-        + pad(7, hp)
-        + pad(7, att)
-        + pad(4, def)
-        + pad(4, sta)
-      );
+        renderRow("Name", "CP", "HP", "Level", "Att", "Def", "Sta", "IV");
+        for (let p of pokemons) {
+            renderRow(p.name, p.cp, p.hp, p.level, p.attack, p.defence, p.stamina, Math.round(p.iv));
+        }
 
-    renderRow("Name", "CP", "HP", "Level", "Att", "Def", "Sta", "IV");
-    for (let p of pokemons) {
-      renderRow(p.name, p.cp, p.hp, p.level, p.attack, p.defence, p.stamina, Math.round(p.iv));
-    }
-    process.exit();
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+        process.exit();
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
